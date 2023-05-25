a2a93a3809e96c8a95d79a69d6a0efe422ae6e90
eomjinyoung <jinyoung.eom@gmail.com>
2023-05-25 16:03:51 +0900

    .

---
+++ java-lang/app/src/main/java/com/eomcs/lang/ex03/Test1.java
@@ -0,0 +1,10 @@
+package com.eomcs.lang.ex03;
+
+public class Test1 {
+  public static void main(String[] args) {
+    final float f = 12.375f;
+    final double d = 12.375;
+    System.out.println(f); 
+    System.out.println(d);
+  }
+}
