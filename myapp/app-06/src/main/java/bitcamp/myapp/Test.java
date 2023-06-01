375455f2533d13f0a520b78d5b6cf022bdb86568
eomjinyoung <jinyoung.eom@gmail.com>
2023-06-01 13:14:22 +0900

    .

---
+++ myapp/app-06/src/main/java/bitcamp/myapp/Test.java
@@ -0,0 +1,10 @@
+package bitcamp.myapp;
+
+public class Test {
+  public static void main(String[] args) {
+    int[] no;
+    no = new int[] { 100, 200, 300 };
+    System.out.println(no.length);
+
+  }
+}
