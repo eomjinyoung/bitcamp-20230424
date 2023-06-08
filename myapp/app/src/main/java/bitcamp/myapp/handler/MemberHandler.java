64557e43df93e231fc8727fdd1ae986ea07b2129
eomjinyoung <jinyoung.eom@gmail.com>
2023-06-08 12:33:41 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/MemberHandler.java
@@ -1,15 +1,12 @@
 package bitcamp.myapp.handler;
 
 import bitcamp.util.Prompt;
+import bitcamp.myapp.vo.Member;
 
 public class MemberHandler {
 
   static final int MAX_SIZE = 100;
-  static int[] no = new int[MAX_SIZE];
-  static String[] name = new String[MAX_SIZE];
-  static String[] email = new String[MAX_SIZE];
-  static String[] password = new String[MAX_SIZE];
-  static char[] gender = new char[MAX_SIZE];
+  static Member[] members = new Member[MAX_SIZE];
   static int userId = 1;
   static int length = 0;
 
@@ -22,13 +19,16 @@ public class MemberHandler {
       return;
     }
 
-    name[length] = Prompt.inputString("이름? ");
-    email[length] = Prompt.inputString("이메일? ");
-    password[length] = Prompt.inputString("암호? ");
-    gender[length] = inputGender((char)0);
+    Member m = new Member();
+    m.name = Prompt.inputString("이름? ");
+    m.email = Prompt.inputString("이메일? ");
+    m.password = Prompt.inputString("암호? ");
+    m.gender = inputGender((char)0);
+    m.no = userId++;
 
-    no[length] = userId++;
-    length++;
+    // 위에서 만든 Member 인스턴스의 주소를 잃어버리지 않게 
+    // 레퍼런스 배열에 담는다.
+    members[length++] = m;
   }
 
   public static void printMembers() {
@@ -37,19 +37,21 @@ public class MemberHandler {
     System.out.println("---------------------------------------");
 
     for (int i = 0; i < length; i++) {
+      Member m = members[i];
       System.out.printf("%d, %s, %s, %s\n", 
-        no[i], name[i], email[i], 
-        toGenderString(gender[i]));
+        m.no, m.name, m.email, 
+        toGenderString(m.gender));
     }
   }
 
   public static void viewMember() {
     String memberNo = Prompt.inputString("번호? ");
     for (int i = 0; i < length; i++) {
-      if (no[i] == Integer.parseInt(memberNo)) {
-        System.out.printf("이름: %s\n", name[i]);
-        System.out.printf("이메일: %s\n", email[i]);
-        System.out.printf("성별: %s\n", toGenderString(gender[i]));
+      Member m = members[i];
+      if (m.no == Integer.parseInt(memberNo)) {
+        System.out.printf("이름: %s\n", m.name);
+        System.out.printf("이메일: %s\n", m.email);
+        System.out.printf("성별: %s\n", toGenderString(m.gender));
         return;
       }
     }
@@ -63,14 +65,15 @@ public class MemberHandler {
   public static void updateMember() {
     String memberNo = Prompt.inputString("번호? ");
     for (int i = 0; i < length; i++) {
-      if (no[i] == Integer.parseInt(memberNo)) {
-        System.out.printf("이름(%s)? ", name[i]);
-        name[i] = Prompt.inputString("");
-        System.out.printf("이메일(%s)? ", email[i]);
-        email[i] = Prompt.inputString("");
+      Member m = members[i];
+      if (m.no == Integer.parseInt(memberNo)) {
+        System.out.printf("이름(%s)? ", m.name);
+        m.name = Prompt.inputString("");
+        System.out.printf("이메일(%s)? ", m.email);
+        m.email = Prompt.inputString("");
         System.out.printf("새암호? ");
-        password[i] = Prompt.inputString("");
-        gender[i] = inputGender(gender[i]);
+        m.password = Prompt.inputString("");
+        m.gender = inputGender(m.gender);
         return;
       }
     }
@@ -111,25 +114,16 @@ public class MemberHandler {
     }
 
     for (int i = deletedIndex; i < length - 1; i++) {
-      no[i] = no[i + 1];
-      name[i] = name[i + 1];
-      email[i] = email[i + 1];
-      password[i] = password[i + 1];
-      gender[i] = gender[i + 1];
+      members[i] = members[i + 1];
     }
 
-    no[length - 1] = 0;
-    name[length - 1] = null;
-    email[length - 1] = null;
-    password[length - 1] = null;
-    gender[length - 1] = (char) 0;
-
-    length--;
+    members[--length] = null;
   }
 
   private static int indexOf(int memberNo) {
     for (int i = 0; i < length; i++) {
-      if (no[i] == memberNo) {
+      Member m = members[i];
+      if (m.no == memberNo) {
         return i;
       }
     }
