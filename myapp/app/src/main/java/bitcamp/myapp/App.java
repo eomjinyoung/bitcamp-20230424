f1a0302f28c2db3e68bf85060f3babeb60864122
eomjinyoung <jinyoung.eom@gmail.com>
2023-06-08 14:27:28 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -48,12 +48,4 @@ public class App {
     System.out.println("나의 목록 관리 시스템");
     System.out.println("----------------------------------");
   }
-
-  static boolean promptContinue() {
-    String response = Prompt.inputString("계속 하시겠습니까?(Y/n) ");
-    if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
-      return false;
-    }
-    return true;
-  }
 }
