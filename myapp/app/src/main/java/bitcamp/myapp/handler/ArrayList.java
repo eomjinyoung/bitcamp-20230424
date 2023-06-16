e802ded8989e305def51b325dfa42a2fe73b6f05
eomjinyoung <jinyoung.eom@gmail.com>
2023-06-16 12:23:37 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/handler/ArrayList.java
+++ myapp/app/src/main/java/bitcamp/myapp/handler/ArrayList.java
@@ -1,71 +1,63 @@
 package bitcamp.myapp.handler;
 
-import bitcamp.myapp.vo.Member;
-
 public class ArrayList {
   private static final int DEFAULT_SIZE = 3;
 
-  private Member[] members = new Member[DEFAULT_SIZE];
+  private Object[] list = new Object[DEFAULT_SIZE];
   private int length;
 
-  public boolean add(Member m) {
-    if (this.length == members.length) {
+  public boolean add(Object obj) {
+    if (this.length == list.length) {
       increase();
     }
-    this.members[this.length++] = m;
+    this.list[this.length++] = obj;
     return true;
   }
 
   private void increase() {
-    Member[] arr = new Member[members.length + (members.length >> 1)];
-    for (int i = 0; i < members.length; i++) {
-      arr[i] = members[i];
+    Object[] arr = new Object[list.length + (list.length >> 1)];
+    for (int i = 0; i < list.length; i++) {
+      arr[i] = list[i];
     }
-    members = arr;
-    System.out.println("배열 확장: " + members.length);
+    list = arr;
+    //System.out.println("배열 확장: " + list.length);
   }
 
-  public Member[] list() {
-    // 리턴할 값을 담을 배열을 생성
-    Member[] arr = new Member[this.length];
-
-    // 원본 배열에서 입력된 인스턴스 주소를 꺼내
-    // 새 배열에 담는다.
+  public Object[] list() {
+    Object[] arr = new Object[this.length];
     for (int i = 0; i < this.length; i++) {
-      arr[i] = this.members[i];
+      arr[i] = this.list[i];
     }
-
-    // 새 배열을 리턴한다.
     return arr;
   }
 
-  public Member get(int no) {
+  public Object get(Object obj) {
     for (int i = 0; i < this.length; i++) {
-      Member m = this.members[i];
-      if (m.getNo() == no) {
-        return m;
+      Object item = this.list[i];
+      if (item.equals(obj)) {
+        return item;
       }
     }
     return null;
   }
 
-  public boolean delete(int no) {
-    int deletedIndex = indexOf(no);
+  public boolean delete(Object obj) {
+    int deletedIndex = indexOf(obj);
     if (deletedIndex == -1) {
       return false;
     }
 
     for (int i = deletedIndex; i < this.length - 1; i++) {
-      this.members[i] = this.members[i + 1];
+      this.list[i] = this.list[i + 1];
     }
-    this.members[--this.length] = null;
+    this.list[--this.length] = null;
     return true;
   }
 
-  private int indexOf(int memberNo) {
+  private int indexOf(Object obj) {
     for (int i = 0; i < this.length; i++) {
-      Member m = this.members[i];
-      if (m.getNo() == memberNo) {
+      Object item = this.list[i];
+      if (item.equals(obj)) {
         return i;
       }
     }
--- myapp/app/src/main/java/bitcamp/test/Test.java
+++ myapp/app/src/main/java/bitcamp/test/Test.java
@@ -3,10 +3,11 @@ package bitcamp.test;
 public class Test {
 
   public static void main(String[] args) {
-    Calculator2 c2 = new Calculator2();
+    Object c = new Calculator2();
 
-    System.out.println(c2.minus(100, 200));
-    System.out.println(c2.plus(100, 200));
+    //Calculator2 c2 = (Calculator2) c;
+
+    System.out.println(((Calculator2)c).minus(100, 200));
+    System.out.println(((Calculator)c).plus(100, 200));
   }
-
 }
