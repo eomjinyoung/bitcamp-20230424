7c00d42ee8f0fe331b115c42cd431377fe7c5bbf
eomjinyoung <jinyoung.eom@gmail.com>
2023-06-28 12:58:32 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -41,6 +41,11 @@ public class App {
     new App().execute();
   }
 
+  static void printTitle() {
+    System.out.println("나의 목록 관리 시스템");
+    System.out.println("----------------------------------");
+  }
+
   public void execute() {
     printTitle();
 
@@ -52,6 +57,50 @@ public class App {
   }
 
   private void loadData() {
+    loadMember();
+    loadBoard();
+    //loadReading();
+  }
+
+  private void saveData() {
+    saveMember();
+    saveBoard();
+    //saveReading();
+  }
+
+  private void prepareMenu() {
+    MenuGroup memberMenu = new MenuGroup("회원");
+    memberMenu.add(new Menu("등록", new MemberAddListener(memberList)));
+    memberMenu.add(new Menu("목록", new MemberListListener(memberList)));
+    memberMenu.add(new Menu("조회", new MemberDetailListener(memberList)));
+    memberMenu.add(new Menu("변경", new MemberUpdateListener(memberList)));
+    memberMenu.add(new Menu("삭제", new MemberDeleteListener(memberList)));
+    mainMenu.add(memberMenu);
+
+    MenuGroup boardMenu = new MenuGroup("게시글");
+    boardMenu.add(new Menu("등록", new BoardAddListener(boardList)));
+    boardMenu.add(new Menu("목록", new BoardListListener(boardList)));
+    boardMenu.add(new Menu("조회", new BoardDetailListener(boardList)));
+    boardMenu.add(new Menu("변경", new BoardUpdateListener(boardList)));
+    boardMenu.add(new Menu("삭제", new BoardDeleteListener(boardList)));
+    mainMenu.add(boardMenu);
+
+    MenuGroup readingMenu = new MenuGroup("독서록");
+    readingMenu.add(new Menu("등록", new BoardAddListener(readingList)));
+    readingMenu.add(new Menu("목록", new BoardListListener(readingList)));
+    readingMenu.add(new Menu("조회", new BoardDetailListener(readingList)));
+    readingMenu.add(new Menu("변경", new BoardUpdateListener(readingList)));
+    readingMenu.add(new Menu("삭제", new BoardDeleteListener(readingList)));
+    mainMenu.add(readingMenu);
+
+    Menu helloMenu = new Menu("안녕!");
+    helloMenu.addActionListener(new HeaderListener());
+    helloMenu.addActionListener(new HelloListener());
+    helloMenu.addActionListener(new FooterListener());
+    mainMenu.add(helloMenu);
+  }
+
+  private void loadMember() {
     try {
       FileInputStream in = new FileInputStream("member.data");
       int size = in.read() << 8;
@@ -63,11 +112,21 @@ public class App {
         Member member = new Member();
         member.setNo(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
 
-        int count = in.read(buf);
-        member.setName(new String(buf, 0, count, "UTF-8"));
+        int length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        member.setName(new String(buf, 0, length, "UTF-8"));
 
+        length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        member.setEmail(new String(buf, 0, length, "UTF-8"));
 
+        length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        member.setPassword(new String(buf, 0, length, "UTF-8"));
 
+        member.setGender((char)(in.read() << 8 | in.read()));
+
+        memberList.add(member);
       }
 
       in.close();
@@ -77,7 +136,43 @@ public class App {
     }
   }
 
-  private void saveData() {
+  private void loadBoard() {
+    try {
+      FileInputStream in = new FileInputStream("board.data");
+      int size = in.read() << 8;
+      size |= in.read();
+
+      byte[] buf = new byte[1000];
+
+      for (int i = 0; i < size; i++) {
+        Member member = new Member();
+        member.setNo(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
+
+        int length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        member.setName(new String(buf, 0, length, "UTF-8"));
+
+        length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        member.setEmail(new String(buf, 0, length, "UTF-8"));
+
+        length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        member.setPassword(new String(buf, 0, length, "UTF-8"));
+
+        member.setGender((char)(in.read() << 8 | in.read()));
+
+        memberList.add(member);
+      }
+
+      in.close();
+
+    } catch (Exception e) {
+      System.out.println("회원 정보를 읽는 중 오류 발생!");
+    }
+  }
+
+  private void saveMember() {
     try {
       FileOutputStream out = new FileOutputStream("member.data");
 
@@ -123,41 +218,51 @@ public class App {
     }
   }
 
-  private void prepareMenu() {
-    MenuGroup memberMenu = new MenuGroup("회원");
-    memberMenu.add(new Menu("등록", new MemberAddListener(memberList)));
-    memberMenu.add(new Menu("목록", new MemberListListener(memberList)));
-    memberMenu.add(new Menu("조회", new MemberDetailListener(memberList)));
-    memberMenu.add(new Menu("변경", new MemberUpdateListener(memberList)));
-    memberMenu.add(new Menu("삭제", new MemberDeleteListener(memberList)));
-    mainMenu.add(memberMenu);
+  private void saveBoard() {
+    try {
+      FileOutputStream out = new FileOutputStream("board.data");
 
-    MenuGroup boardMenu = new MenuGroup("게시글");
-    boardMenu.add(new Menu("등록", new BoardAddListener(boardList)));
-    boardMenu.add(new Menu("목록", new BoardListListener(boardList)));
-    boardMenu.add(new Menu("조회", new BoardDetailListener(boardList)));
-    boardMenu.add(new Menu("변경", new BoardUpdateListener(boardList)));
-    boardMenu.add(new Menu("삭제", new BoardDeleteListener(boardList)));
-    mainMenu.add(boardMenu);
+      // 저장할 데이터의 개수를 먼저 출력한다.
+      int size = memberList.size();
+      out.write(size >> 8);
+      out.write(size);
 
-    MenuGroup readingMenu = new MenuGroup("독서록");
-    readingMenu.add(new Menu("등록", new BoardAddListener(readingList)));
-    readingMenu.add(new Menu("목록", new BoardListListener(readingList)));
-    readingMenu.add(new Menu("조회", new BoardDetailListener(readingList)));
-    readingMenu.add(new Menu("변경", new BoardUpdateListener(readingList)));
-    readingMenu.add(new Menu("삭제", new BoardDeleteListener(readingList)));
-    mainMenu.add(readingMenu);
+      for (Member member : memberList) {
+        int no = member.getNo();
+        out.write(no >> 24);
+        out.write(no >> 16);
+        out.write(no >> 8);
+        out.write(no);
 
-    Menu helloMenu = new Menu("안녕!");
-    helloMenu.addActionListener(new HeaderListener());
-    helloMenu.addActionListener(new HelloListener());
-    helloMenu.addActionListener(new FooterListener());
-    mainMenu.add(helloMenu);
+        byte[] bytes = member.getName().getBytes("UTF-8");
+        // 출력할 바이트의 개수를 2바이트로 표시한다.
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+
+        // 문자열의 바이트를 출력한다.
+        out.write(bytes);
+
+
+        bytes = member.getEmail().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        bytes = member.getPassword().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        char gender = member.getGender();
+        out.write(gender >> 8);
+        out.write(gender);
+      }
+      out.close();
+
+    } catch (Exception e) {
+      System.out.println("회원 정보를 저장하는 중 오류 발생!");
+    }
   }
 
 
-  static void printTitle() {
-    System.out.println("나의 목록 관리 시스템");
-    System.out.println("----------------------------------");
-  }
 }
