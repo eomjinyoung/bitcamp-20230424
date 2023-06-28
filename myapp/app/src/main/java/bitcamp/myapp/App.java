7596bcde83c3ad44ca230f68081f22493b394017
eomjinyoung <jinyoung.eom@gmail.com>
2023-06-28 14:23:46 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -145,24 +145,38 @@ public class App {
       byte[] buf = new byte[1000];
 
       for (int i = 0; i < size; i++) {
-        Member member = new Member();
-        member.setNo(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
+        Board board = new Board();
+        board.setNo(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
 
         int length = in.read() << 8 | in.read();
         in.read(buf, 0, length);
-        member.setName(new String(buf, 0, length, "UTF-8"));
+        board.setTitle(new String(buf, 0, length, "UTF-8"));
 
         length = in.read() << 8 | in.read();
         in.read(buf, 0, length);
-        member.setEmail(new String(buf, 0, length, "UTF-8"));
+        board.setContent(new String(buf, 0, length, "UTF-8"));
 
         length = in.read() << 8 | in.read();
         in.read(buf, 0, length);
-        member.setPassword(new String(buf, 0, length, "UTF-8"));
+        board.setWriter(new String(buf, 0, length, "UTF-8"));
 
-        member.setGender((char)(in.read() << 8 | in.read()));
+        length = in.read() << 8 | in.read();
+        in.read(buf, 0, length);
+        board.setPassword(new String(buf, 0, length, "UTF-8"));
 
-        memberList.add(member);
+        board.setViewCount(in.read() << 24 | in.read() << 16 | in.read() << 8 | in.read());
+
+        board.setCreatedDate(
+            (long)in.read() << 56
+            | (long)in.read() << 48
+            | (long)in.read() << 40
+            | (long)in.read() << 32
+            | (long)in.read() << 24
+            | (long)in.read() << 16
+            | (long)in.read() << 8
+            | in.read());
+
+        boardList.add(board);
       }
 
       in.close();
@@ -223,44 +237,58 @@ public class App {
       FileOutputStream out = new FileOutputStream("board.data");
 
       // 저장할 데이터의 개수를 먼저 출력한다.
-      int size = memberList.size();
+      int size = boardList.size();
       out.write(size >> 8);
       out.write(size);
 
-      for (Member member : memberList) {
-        int no = member.getNo();
+      for (Board board : boardList) {
+        int no = board.getNo();
         out.write(no >> 24);
         out.write(no >> 16);
         out.write(no >> 8);
         out.write(no);
 
-        byte[] bytes = member.getName().getBytes("UTF-8");
-        // 출력할 바이트의 개수를 2바이트로 표시한다.
-        out.write(bytes.length >> 8);
-        out.write(bytes.length);
-
-        // 문자열의 바이트를 출력한다.
-        out.write(bytes);
-
-
-        bytes = member.getEmail().getBytes("UTF-8");
+        byte[] bytes = board.getTitle().getBytes("UTF-8");
         out.write(bytes.length >> 8);
         out.write(bytes.length);
         out.write(bytes);
 
-        bytes = member.getPassword().getBytes("UTF-8");
+
+        bytes = board.getContent().getBytes("UTF-8");
         out.write(bytes.length >> 8);
         out.write(bytes.length);
         out.write(bytes);
 
-        char gender = member.getGender();
-        out.write(gender >> 8);
-        out.write(gender);
+        bytes = board.getWriter().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        bytes = board.getPassword().getBytes("UTF-8");
+        out.write(bytes.length >> 8);
+        out.write(bytes.length);
+        out.write(bytes);
+
+        int viewCount = board.getViewCount();
+        out.write(viewCount >> 24);
+        out.write(viewCount >> 16);
+        out.write(viewCount >> 8);
+        out.write(viewCount);
+
+        long createdDate = board.getCreatedDate();
+        out.write((int)(createdDate >> 56));
+        out.write((int)(createdDate >> 48));
+        out.write((int)(createdDate >> 40));
+        out.write((int)(createdDate >> 32));
+        out.write((int)(createdDate >> 24));
+        out.write((int)(createdDate >> 16));
+        out.write((int)(createdDate >> 8));
+        out.write((int)createdDate);
       }
       out.close();
 
     } catch (Exception e) {
-      System.out.println("회원 정보를 저장하는 중 오류 발생!");
+      System.out.println("게시글 정보를 저장하는 중 오류 발생!");
     }
   }
 
