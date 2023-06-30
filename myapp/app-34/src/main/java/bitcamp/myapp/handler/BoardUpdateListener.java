b4f3bcc4432a79b316c34c5dbc90dbdb44147964
eomjinyoung <jinyoung.eom@gmail.com>
2023-06-30 15:47:34 +0900

    .

---
+++ myapp/app-34/src/main/java/bitcamp/myapp/handler/BoardUpdateListener.java
@@ -0,0 +1,42 @@
+package bitcamp.myapp.handler;
+
+import java.util.List;
+import bitcamp.myapp.vo.Board;
+import bitcamp.util.BreadcrumbPrompt;
+
+public class BoardUpdateListener extends AbstractBoardListener {
+
+  public BoardUpdateListener(List<Board> list) {
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    int boardNo = prompt.inputInt("번호? ");
+
+    Board board = this.findBy(boardNo);
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 없습니다!");
+      return;
+    }
+
+    if (!prompt.inputString("암호? ").equals(board.getPassword())) {
+      System.out.println("암호가 일치하지 않습니다!");
+      return;
+    }
+
+    board.setTitle(prompt.inputString("제목(%s)? ", board.getTitle()));
+    board.setContent(prompt.inputString("내용(%s)? ", board.getContent()));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
