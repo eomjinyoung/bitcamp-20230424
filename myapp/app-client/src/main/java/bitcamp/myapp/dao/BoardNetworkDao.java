946802fa6c0756ed9d1f7929267a3e7b3d75f267
eomjinyoung <jinyoung.eom@gmail.com>
2023-07-06 11:43:26 +0900

    .

--- myapp/app-client/src/main/java/bitcamp/myapp/dao/BoardNetworkDao.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/dao/BoardNetworkDao.java
@@ -2,6 +2,7 @@ package bitcamp.myapp.dao;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import com.google.gson.Gson;
@@ -24,18 +25,20 @@ public class BoardNetworkDao implements BoardDao {
   @Override
   public void insert(Board board) {
     try {
-      // 서버에서 실행할 명령을 보낸다.
-      out.writeUTF(dataName + "/insert");
-
-      // 명령을 실행할 때 사용할 데이터를 보낸다.
       Gson gson = new Gson();
-      String jsonStr = gson.toJson(board);
-      out.writeUTF(jsonStr);
+
+      // 서버에 보낼 명령과 데이터를 Map 객체에 담는다.
+      HashMap<String,Object> request = new HashMap<>();
+      request.put("command", dataName + "/insert");
+      request.put("data", gson.toJson(board));
+
+      // Map 객체에 담은 정보를 JSON 문자열로 변환하여 서버에 보낸다.
+      out.writeUTF(gson.toJson(request));
 
       // 서버에서 보낸 응답을 받는다.
       Map<String,Object> response = gson.fromJson(in.readUTF(), Map.class);
       if (!response.get("status").equals("success")) {
-        throw new RuntimeException((String)response.get("message"));
+        throw new RuntimeException((String)response.get("result"));
       }
     } catch (Exception e) {
       System.out.println(e.getMessage());
@@ -47,16 +50,20 @@ public class BoardNetworkDao implements BoardDao {
   @Override
   public List<Board> list() {
     try {
-      out.writeUTF(dataName + "/list");
-
       Gson gson = new Gson();
+
+      HashMap<String,Object> request = new HashMap<>();
+      request.put("command", dataName + "/list");
+
+      out.writeUTF(gson.toJson(request));
+
       Map<String,Object> response = gson.fromJson(in.readUTF(), Map.class);
       if (!response.get("status").equals("success")) {
-        throw new RuntimeException((String)response.get("message"));
+        throw new RuntimeException((String)response.get("result"));
       }
 
       return gson.fromJson(
-          (String)response.get("data"),
+          (String)response.get("result"),
           TypeToken.getParameterized(List.class, Board.class).getType());
 
     } catch (Exception e) {
