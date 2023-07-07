b7394a0747830525f96376bea48743ec086a740b
eomjinyoung <jinyoung.eom@gmail.com>
2023-07-07 16:01:09 +0900

    .

---
+++ myapp/app-38-client/src/main/java/bitcamp/myapp/ClientApp.java
@@ -0,0 +1,123 @@
+package bitcamp.myapp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import bitcamp.dao.DaoBuilder;
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.dao.MemberDao;
+import bitcamp.myapp.handler.BoardAddListener;
+import bitcamp.myapp.handler.BoardDeleteListener;
+import bitcamp.myapp.handler.BoardDetailListener;
+import bitcamp.myapp.handler.BoardListListener;
+import bitcamp.myapp.handler.BoardUpdateListener;
+import bitcamp.myapp.handler.FooterListener;
+import bitcamp.myapp.handler.HeaderListener;
+import bitcamp.myapp.handler.HelloListener;
+import bitcamp.myapp.handler.MemberAddListener;
+import bitcamp.myapp.handler.MemberDeleteListener;
+import bitcamp.myapp.handler.MemberDetailListener;
+import bitcamp.myapp.handler.MemberListListener;
+import bitcamp.myapp.handler.MemberUpdateListener;
+import bitcamp.net.RequestEntity;
+import bitcamp.util.BreadcrumbPrompt;
+import bitcamp.util.Menu;
+import bitcamp.util.MenuGroup;
+
+public class ClientApp {
+
+  Socket socket;
+  DataOutputStream out;
+  DataInputStream in;
+
+  MemberDao memberDao;
+  BoardDao boardDao;
+  BoardDao readingDao;
+
+  BreadcrumbPrompt prompt = new BreadcrumbPrompt();
+
+  MenuGroup mainMenu = new MenuGroup("메인");
+
+  public ClientApp(String ip, int port) throws Exception {
+
+    this.socket = new Socket(ip, port);
+    this.out = new DataOutputStream(socket.getOutputStream());
+    this.in = new DataInputStream(socket.getInputStream());
+
+    DaoBuilder daoBuilder = new DaoBuilder(in, out);
+
+    this.memberDao = daoBuilder.build("member", MemberDao.class);
+    this.boardDao = daoBuilder.build("board", BoardDao.class);
+    this.readingDao = daoBuilder.build("reading", BoardDao.class);
+
+    prepareMenu();
+  }
+
+  public void close() throws Exception {
+    prompt.close();
+    out.close();
+    in.close();
+    socket.close();
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (args.length < 2) {
+      System.out.println("실행 예) java ... bitcamp.myapp.ClientApp 서버주소 포트번호");
+      return;
+    }
+
+    ClientApp app = new ClientApp(args[0], Integer.parseInt(args[1]));
+    app.execute();
+    app.close();
+  }
+
+  static void printTitle() {
+    System.out.println("나의 목록 관리 시스템");
+    System.out.println("----------------------------------");
+  }
+
+  public void execute() {
+    printTitle();
+    mainMenu.execute(prompt);
+
+    try {
+      out.writeUTF(new RequestEntity().command("quit").toJson());
+
+    } catch (Exception e) {
+      System.out.println("종료 오류!");
+      e.printStackTrace();
+    }
+  }
+
+  private void prepareMenu() {
+    MenuGroup memberMenu = new MenuGroup("회원");
+    memberMenu.add(new Menu("등록", new MemberAddListener(memberDao)));
+    memberMenu.add(new Menu("목록", new MemberListListener(memberDao)));
+    memberMenu.add(new Menu("조회", new MemberDetailListener(memberDao)));
+    memberMenu.add(new Menu("변경", new MemberUpdateListener(memberDao)));
+    memberMenu.add(new Menu("삭제", new MemberDeleteListener(memberDao)));
+    mainMenu.add(memberMenu);
+
+    MenuGroup boardMenu = new MenuGroup("게시글");
+    boardMenu.add(new Menu("등록", new BoardAddListener(boardDao)));
+    boardMenu.add(new Menu("목록", new BoardListListener(boardDao)));
+    boardMenu.add(new Menu("조회", new BoardDetailListener(boardDao)));
+    boardMenu.add(new Menu("변경", new BoardUpdateListener(boardDao)));
+    boardMenu.add(new Menu("삭제", new BoardDeleteListener(boardDao)));
+    mainMenu.add(boardMenu);
+
+    MenuGroup readingMenu = new MenuGroup("독서록");
+    readingMenu.add(new Menu("등록", new BoardAddListener(readingDao)));
+    readingMenu.add(new Menu("목록", new BoardListListener(readingDao)));
+    readingMenu.add(new Menu("조회", new BoardDetailListener(readingDao)));
+    readingMenu.add(new Menu("변경", new BoardUpdateListener(readingDao)));
+    readingMenu.add(new Menu("삭제", new BoardDeleteListener(readingDao)));
+    mainMenu.add(readingMenu);
+
+    Menu helloMenu = new Menu("안녕!");
+    helloMenu.addActionListener(new HeaderListener());
+    helloMenu.addActionListener(new HelloListener());
+    helloMenu.addActionListener(new FooterListener());
+    mainMenu.add(helloMenu);
+  }
+}
---
+++ myapp/app-38-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -0,0 +1,152 @@
+package bitcamp.myapp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.dao.BoardListDao;
+import bitcamp.myapp.dao.MemberDao;
+import bitcamp.myapp.dao.MemberListDao;
+import bitcamp.myapp.vo.Board;
+import bitcamp.myapp.vo.Member;
+import bitcamp.net.RequestEntity;
+import bitcamp.net.ResponseEntity;
+
+public class ServerApp {
+
+  int port;
+  ServerSocket serverSocket;
+
+  MemberDao memberDao = new MemberListDao("member.json");
+  BoardDao boardDao = new BoardListDao("board.json");
+  BoardDao readingDao = new BoardListDao("reading.json");
+
+  public ServerApp(int port) throws Exception {
+    this.port = port;
+  }
+
+  public void close() throws Exception {
+    serverSocket.close();
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (args.length < 1) {
+      System.out.println("실행 예) java ... bitcamp.myapp.ServerApp 포트번호");
+      return;
+    }
+
+    ServerApp app = new ServerApp(Integer.parseInt(args[0]));
+    app.execute();
+    app.close();
+  }
+
+  public void execute() throws Exception {
+    System.out.println("[MyList 서버 애플리케이션]");
+
+    this.serverSocket = new ServerSocket(port);
+    System.out.println("서버 실행 중...");
+
+    Socket socket = serverSocket.accept();
+    DataInputStream in = new DataInputStream(socket.getInputStream());
+    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+    while (true) {
+      RequestEntity request = RequestEntity.fromJson(in.readUTF());
+
+      String command = request.getCommand();
+      System.out.println(command);
+
+      if (command.equals("quit")) {
+        break;
+      }
+
+      ResponseEntity response = new ResponseEntity();
+
+      switch (command) {
+        case "board/list":
+          response.status(ResponseEntity.SUCCESS).result(boardDao.list());
+          break;
+        case "board/insert":
+          boardDao.insert(request.getObject(Board.class));
+          response.status(ResponseEntity.SUCCESS);
+          break;
+        case "board/findBy":
+          Board board = boardDao.findBy(request.getObject(Integer.class));
+          if (board == null) {
+            response.status(ResponseEntity.SUCCESS);
+          } else {
+            response.status(ResponseEntity.SUCCESS).result(board);
+          }
+          break;
+        case "board/update":
+          int value = boardDao.update(request.getObject(Board.class));
+          response.status(ResponseEntity.SUCCESS).result(value);
+          break;
+        case "board/delete":
+          value = boardDao.delete(request.getObject(Integer.class));
+          response.status(ResponseEntity.SUCCESS).result(value);
+          break;
+        case "member/list":
+          response.status(ResponseEntity.SUCCESS).result(memberDao.list());
+          break;
+        case "member/insert":
+          memberDao.insert(request.getObject(Member.class));
+          response.status(ResponseEntity.SUCCESS);
+          break;
+        case "member/findBy":
+          Member member = memberDao.findBy(request.getObject(Integer.class));
+          if (member == null) {
+            response.status(ResponseEntity.SUCCESS);
+          } else {
+            response.status(ResponseEntity.SUCCESS).result(member);
+          }
+          break;
+        case "member/update":
+          value = memberDao.update(request.getObject(Member.class));
+          response.status(ResponseEntity.SUCCESS).result(value);
+          break;
+        case "member/delete":
+          value = memberDao.delete(request.getObject(Integer.class));
+          response.status(ResponseEntity.SUCCESS).result(value);
+          break;
+        case "reading/list":
+          response.status(ResponseEntity.SUCCESS).result(boardDao.list());
+          break;
+        case "reading/insert":
+          boardDao.insert(request.getObject(Board.class));
+          response.status(ResponseEntity.SUCCESS);
+          break;
+        case "reading/findBy":
+          board = boardDao.findBy(request.getObject(Integer.class));
+          if (board == null) {
+            response.status(ResponseEntity.SUCCESS);
+          } else {
+            response.status(ResponseEntity.SUCCESS).result(board);
+          }
+          break;
+        case "reading/update":
+          value = boardDao.update(request.getObject(Board.class));
+          response.status(ResponseEntity.SUCCESS).result(value);
+          break;
+        case "reading/delete":
+          value = boardDao.delete(request.getObject(Integer.class));
+          response.status(ResponseEntity.SUCCESS).result(value);
+          break;
+        default:
+          response.status(ResponseEntity.ERROR).result("해당 명령을 지원하지 않습니다!");
+      }
+
+      out.writeUTF(response.toJson());
+    }
+
+    in.close();
+    out.close();
+    socket.close();
+  }
+}
+
+
+
+
+
