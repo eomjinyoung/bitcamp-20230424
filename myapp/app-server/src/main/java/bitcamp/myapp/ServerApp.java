2b0fc577d467f5e8a5806865672e9d40957f6ca1
eomjinyoung <jinyoung.eom@gmail.com>
2023-07-27 14:47:12 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/ServerApp.java
@@ -2,11 +2,15 @@ package bitcamp.myapp;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.dao.MySQLBoardDao;
@@ -33,6 +37,7 @@ public class ServerApp {
   // 자바 스레드풀 준비
   ExecutorService threadPool = Executors.newFixedThreadPool(2);
 
+  SqlSessionFactory sqlSessionFactory;
   DataSource ds = new DataSource("jdbc:mysql://localhost:3306/studydb", "study", "1111");
   MemberDao memberDao;
   BoardDao boardDao;
@@ -46,9 +51,18 @@ public class ServerApp {
 
     this.port = port;
 
+    // 1) mybatis 설정 파일을 읽어들일 도구를 준비한다.
+    InputStream mybatisConfigIn = Resources.getResourceAsStream("bitcamp/myapp/config/mybatis-config.xml");
+
+    // 2) SqlSessionFactory를 만들어줄 빌더 객체 준비
+    SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+
+    // 3) 빌더 객체를 통해 SqlSessionFactory를 생성
+    sqlSessionFactory = builder.build(mybatisConfigIn);
+
     this.memberDao = new MySQLMemberDao(ds);
-    this.boardDao = new MySQLBoardDao(ds, 1);
-    this.readingDao = new MySQLBoardDao(ds, 2);
+    this.boardDao = new MySQLBoardDao(sqlSessionFactory, ds, 1);
+    this.readingDao = new MySQLBoardDao(sqlSessionFactory, ds, 2);
 
     prepareMenu();
   }
@@ -114,7 +128,7 @@ public class ServerApp {
     mainMenu.add(memberMenu);
 
     MenuGroup boardMenu = new MenuGroup("게시글");
-    boardMenu.add(new Menu("등록", new BoardAddListener(boardDao, ds)));
+    boardMenu.add(new Menu("등록", new BoardAddListener(boardDao, sqlSessionFactory)));
     boardMenu.add(new Menu("목록", new BoardListListener(boardDao)));
     boardMenu.add(new Menu("조회", new BoardDetailListener(boardDao, ds)));
     boardMenu.add(new Menu("변경", new BoardUpdateListener(boardDao, ds)));
@@ -122,7 +136,7 @@ public class ServerApp {
     mainMenu.add(boardMenu);
 
     MenuGroup readingMenu = new MenuGroup("독서록");
-    readingMenu.add(new Menu("등록", new BoardAddListener(readingDao, ds)));
+    readingMenu.add(new Menu("등록", new BoardAddListener(readingDao, sqlSessionFactory)));
     readingMenu.add(new Menu("목록", new BoardListListener(readingDao)));
     readingMenu.add(new Menu("조회", new BoardDetailListener(readingDao, ds)));
     readingMenu.add(new Menu("변경", new BoardUpdateListener(readingDao, ds)));
--- myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardAddListener.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardAddListener.java
@@ -1,21 +1,21 @@
 package bitcamp.myapp.handler;
 
 import java.io.IOException;
+import org.apache.ibatis.session.SqlSessionFactory;
 import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
 import bitcamp.util.ActionListener;
 import bitcamp.util.BreadcrumbPrompt;
-import bitcamp.util.DataSource;
 
 public class BoardAddListener implements ActionListener {
 
   BoardDao boardDao;
-  DataSource ds;
+  SqlSessionFactory sqlSessionFactory;
 
-  public BoardAddListener(BoardDao boardDao, DataSource ds) {
+  public BoardAddListener(BoardDao boardDao, SqlSessionFactory sqlSessionFactory) {
     this.boardDao = boardDao;
-    this.ds = ds;
+    this.sqlSessionFactory = sqlSessionFactory;
   }
 
   @Override
@@ -35,10 +35,10 @@ public class BoardAddListener implements ActionListener {
       //      boardDao.insert(board);
       //      Thread.sleep(5000);
 
-      ds.getConnection().commit();
+      sqlSessionFactory.openSession(false).commit();
 
     } catch (Exception e) {
-      try {ds.getConnection().rollback();} catch (Exception e2) {}
+      try {sqlSessionFactory.openSession(false).rollback();} catch (Exception e2) {}
       throw new RuntimeException(e);
     }
   }
