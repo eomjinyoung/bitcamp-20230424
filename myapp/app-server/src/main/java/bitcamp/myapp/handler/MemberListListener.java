320597f1588ddd2132dd34046968374f3fee9c06
eomjinyoung <jinyoung.eom@gmail.com>
2023-07-28 11:50:47 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberListListener.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/MemberListListener.java
@@ -17,14 +17,13 @@ public class MemberListListener implements ActionListener {
   @Override
   public void service(BreadcrumbPrompt prompt) {
     prompt.println("---------------------------------------");
-    prompt.println("번호, 이름, 이메일, 성별");
+    prompt.println("번호, 이름, 이메일");
     prompt.println("---------------------------------------");
 
     List<Member> list = memberDao.findAll();
     for (Member m : list) {
-      prompt.printf("%d, %s, %s, %s\n",
-          m.getNo(), m.getName(), m.getEmail(),
-          m.getGender() == 'M' ? "남성" : "여성");
+      prompt.printf("%d, %s, %s\n",
+          m.getNo(), m.getName(), m.getEmail());
     }
   }
 
