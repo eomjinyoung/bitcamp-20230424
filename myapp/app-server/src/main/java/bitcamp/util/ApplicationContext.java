f3d5d9cc719908e79378b21023099687abbc241f
eomjinyoung <jinyoung.eom@gmail.com>
2023-07-31 16:59:44 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/util/ApplicationContext.java
+++ myapp/app-server/src/main/java/bitcamp/util/ApplicationContext.java
@@ -1,9 +1,13 @@
 package bitcamp.util;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 import bitcamp.myapp.config.AppConfig;
 
 public class ApplicationContext {
@@ -11,6 +15,16 @@ public class ApplicationContext {
   Map<String,Object> beanContainer = new HashMap<>();
 
   public ApplicationContext(Class<?> configClass) throws Exception {
+
+    processBeanAnnotation(configClass);
+
+    ComponentScan componentScan = configClass.getAnnotation(ComponentScan.class);
+    if (componentScan != null) {
+      processComponentScanAnnotation(componentScan);
+    }
+  }
+
+  private void processBeanAnnotation(Class<?> configClass) throws Exception {
     // 클래스의 기본 생성자를 알아낸다.
     Constructor<?> constructor = configClass.getConstructor();
 
@@ -20,8 +34,15 @@ public class ApplicationContext {
     // 해당 클래스에 정의된 메서드 목록만 가져온다.
     Method[] methods = configClass.getDeclaredMethods();
     for (Method m : methods) {
+
+      // 메서드의 리턴 타입이 없다면 무시한다.
       if (m.getReturnType() == void.class) {
-        // 메서드의 리턴 타입이 없다면 무시한다.
+        continue;
+      }
+
+      // @Bean 애노테이션이 붙지 않은 메서드라면 무시한다.
+      Bean beanAnnotation = m.getAnnotation(Bean.class);
+      if (beanAnnotation == null) {
         continue;
       }
 
@@ -29,9 +50,35 @@ public class ApplicationContext {
       // 즉 오직 값을 리턴하는 메서드만 호출한다.
       Object returnValue = m.invoke(obj);
 
-      // 리턴 값을 메서드 이름으로 빈 컨테이너에 저장한다.
-      beanContainer.put(m.getName(), returnValue);
+      // 메서드가 리턴한 값을 컨테이너에 저장한다.
+      if (beanAnnotation.value().length() > 0) {
+        // 애노테이션에 객체 이름이 지정되어 있다면 그 이름으로 객체를 저장한다.
+        beanContainer.put(beanAnnotation.value(), returnValue);
+      } else {
+        // 애노테이션에 설정된 이름이 없다면 메서드 이름을 사용하여 객체를 저장한다.
+        beanContainer.put(m.getName(), returnValue);
+      }
     }
+  }
+
+  private void processComponentScanAnnotation(ComponentScan componentScan) throws Exception {
+    for (String basePackage : componentScan.basePackages()) {
+      System.out.println(basePackage + "---------------------------------");
+      createBeans(basePackage);
+    }
+  }
+
+  private void createBeans(String basePackage) throws Exception {
+    InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream(basePackage.replaceAll("[.]", "/"));
+    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+    reader
+    .lines()
+    .filter(line -> {
+      System.out.println(line);
+      return false;
+    })
+    .map(line -> getClass(line, basePackage))
+    .collect(Collectors.toSet());
   }
 
   public Object getBean(String name) {
@@ -44,7 +91,9 @@ public class ApplicationContext {
 
   public static void main(String[] args) throws Exception {
     ApplicationContext applicationContext = new ApplicationContext(AppConfig.class);
-
+    for (String name : applicationContext.getBeanNames()) {
+      System.out.println(name);
+    }
   }
 }
 
