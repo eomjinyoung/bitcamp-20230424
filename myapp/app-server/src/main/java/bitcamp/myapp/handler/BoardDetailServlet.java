0482ea93e10291b7470567f73fcb87508eefce94
eomjinyoung <jinyoung.eom@gmail.com>
2023-08-16 11:32:10 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardDetailServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardDetailServlet.java
@@ -39,7 +39,7 @@ public class BoardDetailServlet extends HttpServlet {
       out.println("<p>해당 번호의 게시글이 없습니다!</p>");
 
     } else {
-      out.println("<form action='/board/update' method='post'>");
+      out.println("<form action='/board/update' method='post' enctype='multipart/form-data'>");
       out.printf("<input type='hidden' name='category' value='%d'>\n", board.getCategory());
       out.println("<table border='1'>");
       out.printf("<tr><th style='width:120px;'>번호</th>"
@@ -58,6 +58,8 @@ public class BoardDetailServlet extends HttpServlet {
             + " [<a href='/board/file/delete?category=%d&no=%d'>삭제</a>]"
             + "<br>\n", file.getFilePath(), category, file.getNo());
       }
+
+      out.println("<input type='file' name='files' multiple>");
 
       out.println("</td></tr>");
       out.println("</table>");
--- myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardUpdateServlet.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/BoardUpdateServlet.java
@@ -1,12 +1,20 @@
 package bitcamp.myapp.handler;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import bitcamp.myapp.vo.AttachedFile;
 import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
 
@@ -25,31 +33,56 @@ public class BoardUpdateServlet extends HttpServlet {
       return;
     }
 
-    int category = Integer.parseInt(request.getParameter("category"));
-
-    Board board = new Board();
-    board.setNo(Integer.parseInt(request.getParameter("no")));
-    board.setTitle(request.getParameter("title"));
-    board.setContent(request.getParameter("content"));
-    board.setWriter(loginUser);
-    board.setCategory(category);
-
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
     out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("<head>");
     out.println("<meta charset='UTF-8'>");
-    out.printf("<meta http-equiv='refresh' content='1;url=/board/list?category=%d'>\n", category);
     out.println("<title>게시글</title>");
     out.println("</head>");
     out.println("<body>");
     out.println("<h1>게시글 변경</h1>");
+
     try {
+      List<FileItem> parts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+
+      String uploadDir = request.getServletContext().getRealPath("/upload/board/");
+      ArrayList<AttachedFile> attachedFiles = new ArrayList<>();
+
+      Board board = new Board();
+      board.setWriter(loginUser);
+
+      for (FileItem part : parts) {
+        if (part.isFormField()) {
+          if (part.getFieldName().equals("title")) {
+            board.setTitle(part.getString("UTF-8"));
+          } else if (part.getFieldName().equals("content")) {
+            board.setContent(part.getString("UTF-8"));
+          } else if (part.getFieldName().equals("category")) {
+            board.setCategory(Integer.parseInt(part.getString("UTF-8")));
+          } else if (part.getFieldName().equals("no")) {
+            board.setNo(Integer.parseInt(part.getString("UTF-8")));
+          }
+        } else {
+          String filename = UUID.randomUUID().toString();
+          part.write(new File(uploadDir, filename));
+          AttachedFile attachedFile = new AttachedFile();
+          attachedFile.setFilePath(filename);
+          attachedFiles.add(attachedFile);
+        }
+      }
+      board.setAttachedFiles(attachedFiles);
+
       if (InitServlet.boardDao.update(board) == 0) {
         out.println("<p>게시글이 없거나 변경 권한이 없습니다.</p>");
       } else {
+        // 게시글을 정상적으로 변경했으면, 그 게시글의 첨부파일을 추가한다.
+        int count = InitServlet.boardDao.insertFiles(board);
+        System.out.println(count);
+
         out.println("<p>변경했습니다!</p>");
+        response.setHeader("refresh", "1;url=/board/list?category=" + board.getCategory());
       }
       InitServlet.sqlSessionFactory.openSession(false).commit();
 
