93c393ea223927511280ff58e81a2c32601fb562
eomjinyoung <jinyoung.eom@gmail.com>
2023-08-28 17:47:05 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/App.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/App.java
@@ -30,7 +30,7 @@ public class App {
     // 리턴 값: 등록된 웹 애플리케이션 정보
     StandardContext ctx = (StandardContext) tomcat.addWebapp(
         "/", // 컨텍스트 경로(웹 애플리케이션 경로)
-        new File("src/main/webapp").getAbsolutePath() // 웹 애플리케이션 파일이 있는 실제 경로
+        new File("app-server/src/main/webapp").getAbsolutePath() // 웹 애플리케이션 파일이 있는 실제 경로
         );
 
     // 자바 클래스 파일이 갱신되었을 때 해당 파일을 자동으로 다시 로딩하게 설정.
@@ -43,7 +43,7 @@ public class App {
     resources.addPreResources(new DirResourceSet(
         resources, // 루트 웹 애플리케이션 정보
         "/WEB-INF/classes", // 서블릿 클래스 파일의 위치 정보
-        new File("build/classes/java/main").getAbsolutePath(), // 서블릿 클래스 파일이 있는 실제 경로
+        new File("app-server/build/classes/java/main").getAbsolutePath(), // 서블릿 클래스 파일이 있는 실제 경로
         "/" // 웹 애플리케이션 내부 경로
         ));
 
