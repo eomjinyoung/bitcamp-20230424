aabf02b7a0de1497db42f40f37bd44ad9de66508
eomjinyoung <jinyoung.eom@gmail.com>
2023-09-07 14:54:14 +0900

    .

---
+++ spring-app/app/src/main/java/bitcamp/AppConfig.java
@@ -0,0 +1,7 @@
+package bitcamp;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("bitcamp")
+public class AppConfig {
+}
---
+++ spring-app/app/src/main/java/bitcamp/MyWebApplicationInitializer.java
@@ -0,0 +1,35 @@
+package bitcamp;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.AbstractContextLoaderInitializer;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebListener;
+
+public class MyWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return null;
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[] {AppConfig.class};
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[] {"/app/*"};
+  }
+
+  @Override
+  protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+    registration.setMultipartConfig(new MultipartConfigElement(null, 10000000, 15000000, 1000000));
+  }
+}
